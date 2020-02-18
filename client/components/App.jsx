d74286da6842e844697b1c3ddd0f20a93e171723
student <student@eda-bc-04.localdomain>
2020-02-18 17:59:06 +1300

    start robots

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import robots from '../../robotsData'
+import CardList from './CardList'
+import SearchBox from './searchBox'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange(e) {
+    console.log(e.target.value)
+  }
+  render() {
+    return (
+      <div>
+        <h1>Robo Friends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={robots} />
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ client/components/searchBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBox = ({searchField, searchChange}) => {
+  return (
+    <input
+      type='search'
+      placeholder='search robots'
+      onChange={searchChange}
+      />
+  )
+}
+
+export default SearchBox
